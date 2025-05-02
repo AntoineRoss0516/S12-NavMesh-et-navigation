cd8a54a91faa2f494ec329f7ec7db327dfed3966
AntoineRoss0516 <antoine.ross.16@outlook.com>
2025-05-02 15:23:56 -0400

    E

---
+++ Assets/Scenes/Agent.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Agent : MonoBehaviour
+{
+    [SerializeField] private GameObject _cible;
+    private NavMeshAgent _agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+    _agent=GetComponent<NavMeshAgent>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    _agent.SetDestination(_cible.transform.position);    
+    }
+}
